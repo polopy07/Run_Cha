4c410d6e92fb9c12707aacfdc716cc25ca439e3e
웅재 <polopy07@gmail.com>
2026-05-14 12:44:57 +0900

    fix: Jest transformIgnorePatterns에 ESM 패키지 추가
    @react-navigation/native 등 ESM으로 배포된 패키지를
    Jest가 Babel로 변환하도록 transformIgnorePatterns 설정 추가
    
    - SyntaxError: Unexpected token 'export' 오류 해결
    - GitHub CI 테스트 통과 확인

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  transformIgnorePatterns: [
+    'node_modules/(?!(' +
+      '@react-navigation|' +
+      '@react-native|' +
+      'react-native|' +
+      'react-native-safe-area-context|' +
+      '@rneui' +
+    ')/)',
+  ],
 };
