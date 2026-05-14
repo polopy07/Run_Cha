62ad32944e1dea71b3a39058e6da0f0b3a9f284a
polopy07 <polopy07@gmail.com>
2026-05-14 17:42:22 +0900

    fix: jest에서 react-native-gesture-handler 모킹 추가 @react-navigation/stack이 내부적으로 의존하는 gesture-handler가 jest 환경에서 네이티브 모듈을 찾지 못해 App.test 실패하던 문제 해결.

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   moduleNameMapper: {
     'react-native-maps': '<rootDir>/__mocks__/react-native-maps.js',
     'react-native-geolocation-service': '<rootDir>/__mocks__/react-native-geolocation-service.js',
