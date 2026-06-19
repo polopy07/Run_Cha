79c1b788f2a46ee58d597ac74e8e0cee9cddf304
polopy07 <163103055+polopy07@users.noreply.github.com>
2026-06-19 20:59:38 +0900

    fix(ios): revert unnecessary GoogleMaps import (iOS uses Apple Maps)

--- apps/mobile/ios/mobile/AppDelegate.swift
+++ apps/mobile/ios/mobile/AppDelegate.swift
@@ -2,7 +2,6 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
-import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,9 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-    // TODO: Replace with new Google Maps API key from Google Cloud Console
-    GMSServices.provideAPIKey("REPLACE_WITH_YOUR_GOOGLE_MAPS_API_KEY")
-
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
@@ -50,4 +46,3 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
 #endif
   }
 }
-
